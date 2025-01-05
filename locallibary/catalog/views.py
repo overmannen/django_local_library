77162620a80b38090872be4ab41777fbbc7ec78f
overmannen <jesper231@gmail.com>
2025-01-05 20:59:47 +0100

    adding views.py

--- locallibary/catalog/views.py
+++ locallibary/catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Book, BookInstance, Author, Genre, Language
+
 
 # Create your views here.
+def index(request):
+    num_books = Book.objects.all().count()
+    num_authors = Author.objects.all().count()
+    num_instances = BookInstance.objects.count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+    }
+
+    return render(request, "index.html", {"context": context})
