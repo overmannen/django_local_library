8680e067ac511bce2f6cec0989935ccc2db0151c
overmannen <jesper231@gmail.com>
2025-01-18 17:44:36 +0100

    changing/delete authors/books

--- locallibary/catalog/urls.py
+++ locallibary/catalog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path, include
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -10,5 +10,14 @@ urlpatterns = [
         r"^author/(?P<pk>\d+)$", views.AuthorDetailView.as_view(), name="author-detail"
     ),
     path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
-    path("borrowed/", views.LoanedBooksAllListView.as_view(), name="borrowed"),
+    path("borrowed/", views.LoanedBooksAllListView.as_view(), name="all-borrowed"),
+    path(
+        "book/<uuid:pk>/renew/", views.renew_book_librarian, name="renew-book-librarian"
+    ),
+    path("author/create/", views.AuthorCreate.as_view(), name="author-create"),
+    path("author/<int:pk>/update/", views.AuthorUpdate.as_view(), name="author-update"),
+    path("author/<int:pk>/delete/", views.AuthorDelete.as_view(), name="author-delete"),
+    path("book/create/", views.BookCreate.as_view(), name="book-create"),
+    path("author/<int:pk>/update", views.BookUpdate.as_view(), name="book-update"),
+    path("author/<int:pk>/delete", views.BookDelete.as_view(), name="book-delete"),
 ]
